b63b6d7507cf92c2c827bfa94589d040166079d1
FleetForce Developer <crewing@fleetforce.ru>
2026-07-31 09:47:30 +0300

    Remove temp inspect script

--- _inspect_zip.ps1
+++
@@ -1,8 +0,0 @@
-Add-Type -Assembly 'System.IO.Compression.FileSystem'
-try {
-    $zip = [System.IO.Compression.ZipFile]::OpenRead('c:\Users\User\Documents\fleetforce\fleetforce.zip')
-    $zip.Entries | Select-Object FullName, Length | Format-Table
-    $zip.Dispose()
-} catch {
-    Write-Host "ZIP ERROR: $($_.Exception.Message)"
-}
