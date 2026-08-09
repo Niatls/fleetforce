0c520e2f7c72cbf28e8fc5923ed71daec11873e2
Madao <asdqwest93@gmail.com>
2026-08-10 02:58:37 +0300

    style: reduce BUILD YEAR column width to exact text width (15mm) and expand dateFrom, vesselName, flag, crewingAgent columns

--- src/components/Admin/exportDocUtils.js
+++ src/components/Admin/exportDocUtils.js
@@ -170,19 +170,19 @@ export const buildApplicationFormHtml = (cand) => {
     const s = (cand.seaService || [])[i] || {};
     return `
       <tr style="height:6mm;">
-        <td style="width:11.80mm;background-color:#FFFFFF;height:6mm;">${v(s.dateFrom)}</td>
-        <td style="width:25.88mm;background-color:#FFFFFF;height:6mm;" colspan="2">${v(s.dateTo)}</td>
-        <td style="width:14.08mm;background-color:#FFFFFF;height:6mm;">${v(s.rankHeld)}</td>
-        <td style="width:14.08mm;background-color:#FFFFFF;height:6mm;">${s.salary ? '$' + s.salary : '&nbsp;'}</td>
-        <td style="width:14.08mm;background-color:#FFFFFF;height:6mm;">${v(s.vesselName)}</td>
-        <td style="width:42.23mm;background-color:#FFFFFF;height:6mm;" colspan="3">${v(s.shipowner)}</td>
-        <td style="width:28.16mm;background-color:#FFFFFF;height:6mm;" colspan="2">${v(s.vesselType)}</td>
-        <td style="width:42.23mm;background-color:#FFFFFF;height:6mm;" colspan="3">${v(s.engineType)}</td>
-        <td style="width:28.16mm;background-color:#FFFFFF;height:6mm;" colspan="2">${v(s.buildYear)}</td>
-        <td style="width:14.08mm;background-color:#FFFFFF;height:6mm;">${v(s.dwtGrt)}</td>
-        <td style="width:14.08mm;background-color:#FFFFFF;height:6mm;">${v(s.engineBhp)}</td>
-        <td style="width:14.08mm;background-color:#FFFFFF;height:6mm;">${v(s.flag)}</td>
-        <td style="width:14.08mm;background-color:#FFFFFF;height:6mm;">${v(s.manningCompany)}</td>
+        <td style="width:27.00mm;background-color:#FFFFFF;height:6mm;" colspan="2">${v(s.dateFrom)}</td>
+        <td style="width:28.00mm;background-color:#FFFFFF;height:6mm;" colspan="2">${v(s.dateTo)}</td>
+        <td style="width:15.00mm;background-color:#FFFFFF;height:6mm;">${v(s.rankHeld)}</td>
+        <td style="width:13.00mm;background-color:#FFFFFF;height:6mm;">${s.salary ? '$' + s.salary : '&nbsp;'}</td>
+        <td style="width:30.00mm;background-color:#FFFFFF;height:6mm;" colspan="2">${v(s.vesselName)}</td>
+        <td style="width:28.00mm;background-color:#FFFFFF;height:6mm;" colspan="2">${v(s.shipowner)}</td>
+        <td style="width:28.00mm;background-color:#FFFFFF;height:6mm;" colspan="2">${v(s.vesselType)}</td>
+        <td style="width:28.00mm;background-color:#FFFFFF;height:6mm;" colspan="2">${v(s.engineType)}</td>
+        <td style="width:15.00mm;background-color:#FFFFFF;height:6mm;">${v(s.buildYear)}</td>
+        <td style="width:14.00mm;background-color:#FFFFFF;height:6mm;">${v(s.dwtGrt)}</td>
+        <td style="width:14.00mm;background-color:#FFFFFF;height:6mm;">${v(s.engineBhp)}</td>
+        <td style="width:15.00mm;background-color:#FFFFFF;height:6mm;">${v(s.flag)}</td>
+        <td style="width:19.00mm;background-color:#FFFFFF;height:6mm;">${v(s.manningCompany)}</td>
       </tr>
     `;
   }).join('');
@@ -191,9 +191,9 @@ export const buildApplicationFormHtml = (cand) => {
     const e = (cand.employers || [])[i] || {};
     return `
       <tr style="height:6mm;">
-        <td style="width:93.99mm;background-color:#FFFFFF;height:6mm;" colspan="7">${v(e.company)}</td>
-        <td style="width:126.70mm;background-color:#FFFFFF;height:6mm;" colspan="9">${v(e.personInCharge)}</td>
-        <td style="width:56.31mm;background-color:#FFFFFF;height:6mm;" colspan="4">${v(e.contactDetails)}</td>
+        <td style="width:98.00mm;background-color:#FFFFFF;height:6mm;" colspan="7">${v(e.company)}</td>
+        <td style="width:114.00mm;background-color:#FFFFFF;height:6mm;" colspan="8">${v(e.personInCharge)}</td>
+        <td style="width:60.00mm;background-color:#FFFFFF;height:6mm;" colspan="4">${v(e.contactDetails)}</td>
       </tr>
     `;
   }).join('');
@@ -497,57 +497,57 @@ export const buildApplicationFormHtml = (cand) => {
 <div class="page-landscape">
 <table>
   <colgroup>
-    <col style="width:11.80mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
-    <col style="width:14.08mm">
+    <col style="width:13.50mm">
+    <col style="width:13.50mm">
+    <col style="width:14.00mm">
+    <col style="width:14.00mm">
+    <col style="width:15.00mm">
+    <col style="width:13.00mm">
+    <col style="width:15.00mm">
+    <col style="width:15.00mm">
+    <col style="width:14.00mm">
+    <col style="width:14.00mm">
+    <col style="width:14.00mm">
+    <col style="width:14.00mm">
+    <col style="width:14.00mm">
+    <col style="width:14.00mm">
+    <col style="width:15.00mm">
+    <col style="width:14.00mm">
+    <col style="width:14.00mm">
+    <col style="width:15.00mm">
+    <col style="width:19.00mm">
   </colgroup>
   <tr>
-    <td style="width:277.00mm;background-color:#A8D08D;font-size:10.0pt;font-weight:bold;text-align:center;" colspan="20">PREVIOUS SEA SERVICE</td>
+    <td style="width:272.00mm;background-color:#A8D08D;font-size:10.0pt;font-weight:bold;text-align:center;" colspan="19">PREVIOUS SEA SERVICE</td>
   </tr>
   <tr>
-    <td style="width:11.80mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">FROM</td>
-    <td style="width:25.88mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;" colspan="2">TO</td>
-    <td style="width:14.08mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">POSITION</td>
-    <td style="width:14.08mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">SALARY</td>
-    <td style="width:14.08mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">NAME OF VESSEL</td>
-    <td style="width:42.23mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;" colspan="3">SHIPOWNER</td>
-    <td style="width:28.16mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;" colspan="2">TYPE OF VESSEL</td>
-    <td style="width:42.23mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;" colspan="3">TYPE OF ENGINE</td>
-    <td style="width:28.16mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;" colspan="2">BUILD YEAR</td>
-    <td style="width:14.08mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">DWT</td>
-    <td style="width:14.08mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">BHP</td>
-    <td style="width:14.08mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">FLAG</td>
-    <td style="width:14.08mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">CREWING AGENT</td>
+    <td style="width:27.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;" colspan="2">FROM</td>
+    <td style="width:28.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;" colspan="2">TO</td>
+    <td style="width:15.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">POSITION</td>
+    <td style="width:13.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">SALARY</td>
+    <td style="width:30.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;" colspan="2">NAME OF VESSEL</td>
+    <td style="width:28.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;" colspan="2">SHIPOWNER</td>
+    <td style="width:28.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;" colspan="2">TYPE OF VESSEL</td>
+    <td style="width:28.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;" colspan="2">TYPE OF ENGINE</td>
+    <td style="width:15.00mm;background-color:#C5E0B3;font-size:8.5pt;font-weight:bold;white-space:nowrap;">BUILD YEAR</td>
+    <td style="width:14.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">DWT</td>
+    <td style="width:14.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">BHP</td>
+    <td style="width:15.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">FLAG</td>
+    <td style="width:19.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;">CREWING AGENT</td>
   </tr>
   ${seaRows}
   <tr>
-    <td style="width:277.00mm;background-color:#A8D08D;font-size:9.0pt;font-weight:bold;text-align:center;" colspan="20">BRIEF INFORMATION ABOUT PREVIOUS EMPLOYERS</td>
+    <td style="width:272.00mm;background-color:#A8D08D;font-size:9.0pt;font-weight:bold;text-align:center;" colspan="19">BRIEF INFORMATION ABOUT PREVIOUS EMPLOYERS</td>
   </tr>
   <tr>
-    <td style="width:93.99mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;text-align:center;" colspan="7">COMPANY</td>
-    <td style="width:126.70mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;text-align:center;" colspan="9">PERSON IN CHARGE</td>
-    <td style="width:56.31mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;text-align:center;" colspan="4">CONTACT DETAILS (Phone Number, e-mail)</td>
+    <td style="width:98.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;text-align:center;" colspan="7">COMPANY</td>
+    <td style="width:114.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;text-align:center;" colspan="8">PERSON IN CHARGE</td>
+    <td style="width:60.00mm;background-color:#C5E0B3;font-size:9.0pt;font-weight:bold;text-align:center;" colspan="4">CONTACT DETAILS (Phone Number, e-mail)</td>
   </tr>
   ${empRows}
   <tr>
-    <td style="width:23.60mm;background-color:#FFFFFF;" colspan="2"></td>
-    <td style="width:253.40mm;background-color:#FFFFFF;font-size:9.0pt;" colspan="18">I hereby confirm that above information is true and correct to the best of my knowledge. I understand that this information will be held in the computer database due to my real or possible employment. Signing it, I willfully give my permission to collect and process my personal information and to use it in all and legal way. I give my permission for my personal information to be provided to the possible employers and any other persons, if such need arises for my employment. Besides, I permit the Company employees to request personal information (data) about me from my former employers.</td>
+    <td style="width:27.00mm;background-color:#FFFFFF;" colspan="2"></td>
+    <td style="width:245.00mm;background-color:#FFFFFF;font-size:9.0pt;" colspan="17">I hereby confirm that above information is true and correct to the best of my knowledge. I understand that this information will be held in the computer database due to my real or possible employment. Signing it, I willfully give my permission to collect and process my personal information and to use it in all and legal way. I give my permission for my personal information to be provided to the possible employers and any other persons, if such need arises for my employment. Besides, I permit the Company employees to request personal information (data) about me from my former employers.</td>
   </tr>
   <tr>
     <td style="width:11.80mm;background-color:#C5E0B3;font-size:10.0pt;font-weight:bold;">Date:</td>
